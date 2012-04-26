1f7c607b27f82728fe5f1b1bbf81bba025f3a240
Ziv Scully <zivscully@gmail.com>
2012-04-26 05:49:59 -0400

    Replaced shooter encoder, changed shooter constants.

--- src/edu/neu/nutrons/reboundrumble/subsystems/Shooter.java
+++ src/edu/neu/nutrons/reboundrumble/subsystems/Shooter.java
@@ -16,21 +16,21 @@ import edu.wpi.first.wpilibj.command.PIDSubsystem;
 public class Shooter extends PIDSubsystem {
 
     // TODO: tune PID.
-    private static final double kp = 3.3E-6;
+    private static final double kp = 1.0E-5;//3.3E-6;
     private static final double ki = 0.0;
-    private static final double kd = 3.025E-5;
+    private static final double kd = 0.0;//3.025E-5;
     public static final double MANUAL_POWER_INC = 0.02;
     public static final double MANUAL_RATE_INC = 200.0;
     public static final double FENDER_POWER = 0.56;
     public static final double KEY_POWER = 0.94;
     public static final double FENDER_RATE = 9800.0;
     public static final double KEY_RATE = 15400.0;
-    private final double ENC_SCALE = -1.0;
+    private final double ENC_SCALE = -7.2;
     public static final double RATE_SETTLE_TIMEOUT = 2.0;
     private final int ENC_AVG_LENGTH = 5;
-    private final int POWER_AVG_LENGTH = 10;
-    private final int SETPOINT_DEBOUNCE_LENGTH = 30;
-    private final double MAX_BACKWARD_POWER = 0.2;
+    private final int POWER_AVG_LENGTH = 5;
+    private final int SETPOINT_DEBOUNCE_LENGTH = 10;
+    private final double MAX_BACKWARD_POWER = 0.0;
     private final double TOLERANCE = 150;
 
     // Actual robot parts.
